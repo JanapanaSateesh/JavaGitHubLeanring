7b47ec8e8b21eb4797389b1c60f72b6ef9b1a9d6
JanapanaSateesh <47514442+JanapanaSateesh@users.noreply.github.com>
2025-10-07 20:51:27 +0530

    Update App.java

--- src/main/java/Automation/PracticeAutomation/App.java
+++ src/main/java/Automation/PracticeAutomation/App.java
@@ -6,6 +6,7 @@ package Automation.PracticeAutomation;
  */
 public class App 
 {
+    //Diwali Sales offers and Discounts
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
