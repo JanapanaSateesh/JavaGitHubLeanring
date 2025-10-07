b40876c6e60bb7cea6bb7fca3090a776f5123479
JanapanaSateesh <47514442+JanapanaSateesh@users.noreply.github.com>
2025-10-07 20:51:05 +0530

    Update App.java

--- src/main/java/Automation/PracticeAutomation/App.java
+++ src/main/java/Automation/PracticeAutomation/App.java
@@ -6,6 +6,7 @@ package Automation.PracticeAutomation;
  */
 public class App 
 {
+    //Added offers and discounts for oct 10th big billion day
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
